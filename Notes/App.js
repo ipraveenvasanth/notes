2c522ceb784d702d12d8ecf4712c8983add84afd
praveenkumar <ipraveenphysics28@gmail.com>
2023-01-13 21:39:35 +0530

    async storage data

---
+++ Notes/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#0080ff'
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold'
+          }
+        }}
+      >
+        <Stack.Screen
+          name="Login" component={Login} options={{ headerShown: false, }}
+        />
+        <Stack.Screen
+          name="Home" component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
---
+++ Notes/src/utils/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    CustomFont: {
+        fontWeight: 'bold'
+        //fontFamily: 'DancingScript-Regular'
+    },
+    ButtonText: {
+       // fontFamily: 'AbrilFatface-Regular',
+        fontSize: 30,
+        padding: 10,
+    }
+});
